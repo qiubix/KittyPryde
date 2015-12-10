0b5238cfcab0c32f6d1c82bbcad950207c7ef57f
qiubix <qiubix@gmail.com>
2015-12-10 21:26:00 +0100

    Implement first test

---
+++ PrimeFactors.hpp
@@ -0,0 +1,14 @@
+#ifndef PRIME_FACTORS_HPP
+#define PRIME_FACTORS_HPP
+
+using std::vector;
+
+class PrimeFactors {
+public:
+  vector<int> generate(int) {
+    vector<int> factors;
+    return factors;
+  }
+};
+
+#endif //PRIME_FACTORS_HPP
--- PrimeFactorsTest.cpp
+++ PrimeFactorsTest.cpp
@@ -2,7 +2,17 @@
 #include "PrimeFactors.hpp"
 
 using namespace testing;
+using std::vector;
 
-TEST(PrimeFactorsTest, shouldTestNothing) {
-  ASSERT_THAT(1, Eq(1));
+class PrimeFactorsTest : public Test {
+public:
+  vector<int> list() {
+    vector<int> factors;
+    return factors;
+  }
+  PrimeFactors primeFactors;
+};
+
+TEST_F(PrimeFactorsTest, testOne) {
+  ASSERT_THAT(primeFactors.generate(1), Eq(list()));
 }
