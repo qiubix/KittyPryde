d182368e2cc6b2602c1511f3cdf3932dddfc4b0d
qiubix <qiubix@gmail.com>
2015-12-11 23:13:00 +0100

    Add second test

--- PrimeFactorsTest.cpp
+++ PrimeFactorsTest.cpp
@@ -6,12 +6,19 @@ using std::vector;
 
 class PrimeFactorsTest : public Test {
 public:
-  vector<int> list() {
-    return vector<int> {};
+  vector<int> list(std::initializer_list<int> ints) {
+    vector<int> list;
+    for (int i : ints)
+      list.push_back(i);
+    return list;
   }
   PrimeFactors primeFactors;
 };
 
 TEST_F(PrimeFactorsTest, testOne) {
-  ASSERT_THAT(primeFactors.generate(1), Eq(list()));
+  ASSERT_THAT(primeFactors.generate(1), Eq(list({})));
+}
+
+TEST_F(PrimeFactorsTest, testTwo) {
+  ASSERT_THAT(primeFactors.generate(2), Eq(list({2})));
 }
